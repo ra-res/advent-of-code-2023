81574066df6d0b242222684e3b30292684d9d290
ra-res <ra-res@users.noreply.github.com>
2023-12-14 22:18:20 +0000

    feat(day6): add day6 boilerplate

---
+++ src/days/day06.rs
@@ -0,0 +1,7 @@
+pub fn solve_part_one(lines: Vec<String>) {
+    println!("day6_1 {}", 1)
+}
+
+pub fn solve_part_two(lines: Vec<String>) {
+    println!("day6_2 {}", 1);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,70 @@
 mod days;
 
-use std::fs::read_to_string;
-use std::path::Path;
+#[allow(dead_code)]
+mod main {
+    use crate::days::{day01, day02, day03, day04, day05, day06};
+    use std::fs::read_to_string;
+    use std::path::Path;
 
-use days::{day01, day02, day03, day04, day05};
+    fn read_lines(relative_path: &str) -> Vec<String> {
+        let mut result = Vec::new();
+        let path = Path::new(relative_path);
 
-fn read_lines(relative_path: &str) -> Vec<String> {
-    let mut result = Vec::new();
-    let path = Path::new(relative_path);
+        for line in read_to_string(&path).unwrap().lines() {
+            result.push(line.to_string())
+        }
 
-    for line in read_to_string(&path).unwrap().lines() {
-        result.push(line.to_string())
+        result
     }
 
-    result
-}
+    pub fn day01() {
+        // 54927
+        day01::solve_part_one(read_lines("src/inputs/day01.txt"));
+        // 54581
+        day01::solve_part_two(read_lines("src/inputs/day01.txt"));
+    }
 
-fn day01() {
-    // 54927
-    day01::solve_part_one(read_lines("src/inputs/day01.txt"));
-    // 54581
-    day01::solve_part_two(read_lines("src/inputs/day01.txt"));
-}
+    pub fn day02() {
+        // 2331
+        day02::solve_part_one(read_lines("src/inputs/day02.txt"));
+        // 71585
+        day02::solve_part_two(read_lines("src/inputs/day02.txt"));
+    }
 
-fn day02() {
-    // 2331
-    day02::solve_part_one(read_lines("src/inputs/day02.txt"));
-    // 71585
-    day02::solve_part_two(read_lines("src/inputs/day02.txt"));
-}
+    pub fn day03() {
+        // 2331
+        day03::solve_part_one(read_lines("src/inputs/day03.txt"));
+        // 71585
+        day03::solve_part_two(read_lines("src/inputs/day03.txt"));
+    }
 
-fn day03() {
-    // 2331
-    day03::solve_part_one(read_lines("src/inputs/day03.txt"));
-    // 71585
-    day03::solve_part_two(read_lines("src/inputs/day03.txt"));
-}
+    pub fn day04() {
+        // 20117
+        day04::solve_part_one(read_lines("src/inputs/day04.txt"));
+        // 13768818
+        day04::solve_part_two(read_lines("src/inputs/day04.txt"));
+    }
 
-fn day04() {
-    // 20117
-    day04::solve_part_one(read_lines("src/inputs/day04.txt"));
-    // 13768818
-    day04::solve_part_two(read_lines("src/inputs/day04.txt"));
-}
+    pub fn day05() {
+        // 389056265
+        day05::solve_part_one(read_lines("src/inputs/day05.txt"));
+        // 137516820
+        day05::solve_part_two(read_lines("src/inputs/day05.txt"));
+    }
 
-fn day05() {
-    // 389056265
-    day05::solve_part_one(read_lines("src/inputs/day05.txt"));
-    // 13768818
-    day05::solve_part_two(read_lines("src/inputs/day05.txt"));
+    pub fn day06() {
+        // 389056265
+        day06::solve_part_one(read_lines("src/inputs/day06.test.txt"));
+        // 137516820
+        day06::solve_part_two(read_lines("src/inputs/day06.test.txt"));
+    }
 }
 
 fn main() {
-    day01();
-    day02();
-    day03();
-    day04();
-    day05();
+    // day01();
+    // day02();
+    // day03();
+    // day04();
+    // day05();
+    main::day06();
 }
